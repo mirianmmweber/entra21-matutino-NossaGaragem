d72185af99b56734c36c1e2d840a563d5067567f
mirianmmweber <mirianweber76@gmail.com>
2019-07-11 10:52:01 -0300

    *Exemplo passado em sala

---
+++ NossaGaragem/Model/Veiculo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Veiculo
+    {
+        public int Id;
+        public string Modelo;
+        public decimal Valor;
+
+        //Propriedades para a coluna doo id_categoria(FK)
+        public int IdCategoria;
+        /* Objeto da categoria que permitirá acessar as 
+         * informações da categoria através do veículo.
+         */
+        public Categoria Categoria;
+    }
+}
