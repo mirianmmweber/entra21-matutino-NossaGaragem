c79aeb157fa6cefcc181543dd9e6e5742398d88d
mirianmmweber <mirianweber76@gmail.com>
2019-07-12 11:16:18 -0300

    Exemplo de Sala

--- NossaGaragem/Repository/Estrutura.sql
+++ NossaGaragem/Repository/Estrutura.sql
@@ -1,1 +1,25 @@
-﻿
+﻿CREATE TABLE categorias(
+id INT PRIMARY KEY IDENTITY(1,1),
+nome VARCHAR(100)
+);
+
+DROP TABLE veiculos;
+CREATE TABLE veiculos(
+id INT PRIMARY KEY IDENTITY(1,1),
+
+id_categoria INT, FOREIGN KEY(id_categoria) REFERENCES categorias(id),
+
+modelo VARCHAR(100),
+valor DECIMAL(8,2)
+);
+
+SELECT 
+veiculos.id AS 'VeiculoId',
+veiculos.modelo AS 'VeiculoModelo',
+veiculos.valor AS 'VeiculoValor',
+veiculos.id_categoria AS 'VeiculoIdCategoria',
+categorias.Nome AS 'CategoriaNome'
+FROM veiculos
+INNER JOIN categorias ON 
+(veiculos.id_categoria = id_categoria)
+;
--- NossaGaragem/View/Controllers/CategoriaController.cs
+++ NossaGaragem/View/Controllers/CategoriaController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Model;
+using Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,41 @@ namespace View.Controllers
 {
     public class CategoriaController : Controller
     {
+        private CategoriaRepository repository;
+
+        // Construtor tem como objetivo inicializar
+        // objetos ou tipos primitivos que 
+        // para o devido funcionamento da classe
+        // Sempre executado o contrutor ao 
+        // instanciar um objeto da classe ou seja
+        // ao fazer 'new CategoriaController()'
+        public CategoriaController()
+        {
+            repository = new CategoriaRepository();
+        }
+
         // GET: Categoria
         public ActionResult Index()
         {
+            List<Categoria> categorias = repository.ObterTodos();
+            ViewBag.Categorias = categorias;
+
+
             return View();
+        }
+
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public ActionResult Store(string nome)
+        {
+            Categoria categoria = new Categoria();
+            categoria.Nome = nome;
+            repository.Inserir(categoria);
+            return RedirectToAction("Index");
+
         }
     }
 }
